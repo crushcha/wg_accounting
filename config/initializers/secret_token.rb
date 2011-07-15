18d4a9d7cf0a0e9dbd8797e5eafdae47eb27e852
Chakrit <mail@chakrit.ch>
2011-07-15 13:00:33 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WgAccounting::Application.config.secret_token = 'a99109b3acf77a282d856beb63a5419a0a25368f5172fb7ba3c1026ecee071b684b3129f2214bc7ca2d1e9ba780bab0b9584c95012455efe30880f21a1642962'
